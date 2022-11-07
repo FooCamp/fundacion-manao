3afbbab437caf47a2c9cc5b313b22be670ae5ef3
Juan Camilo Monsalve <jmonsalve858@gmail.com>
2022-11-07 14:16:39 -0500

    [Feature] Adding  the Hover functionality and fixe the variable colors.

--- src/js/_aboutus.js
+++ src/js/_aboutus.js
@@ -1,11 +1,22 @@
 const CLASSES = {
   active: 'active',
 }
-
 const main = () => {
-  const person_master = document.querySelector('.person_card__cards_grid')
-  const person_cardjs = person_master.querySelector('.person_card__container')
-  person_cardjs.addEventListener('mouseover', () => {})
-  person_cardjs.classList.add(CLASSES.active)
+  function getBlockQuote(item) {
+    return item.querySelector('.person_card__quote_image')
+  }
+
+  const person_cards = document.querySelectorAll('.person_card__container')
+  getBlockQuote(person_cards[0]).classList.add(CLASSES.active)
+  person_cards.forEach(function (item) {
+    item.addEventListener('mouseenter', () => {
+      getBlockQuote(person_cards[0]).classList.remove(CLASSES.active)
+      getBlockQuote(item).classList.add(CLASSES.active)
+    })
+    item.addEventListener('mouseleave', () => {
+      getBlockQuote(item).classList.remove(CLASSES.active)
+      getBlockQuote(person_cards[0]).classList.add(CLASSES.active)
+    })
+  })
 }
 main()
