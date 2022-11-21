0575a8df1d857ed84614f0278532ca58aea137a0
Ana Perez <annamarriaperez@gmail.com>
2022-11-21 12:26:03 -0500

    Add mediaquery on javascripts donation modal

--- src/js/_header.js
+++ src/js/_header.js
@@ -1,3 +1,5 @@
+import breakp from '../scss/abstracts/_mixins.scss'
+
 const CLASSES = {
   active: 'active',
   openMobileMenu: 'lock-scroll',
@@ -13,37 +15,37 @@ const SELECTORS = {
   body: 'body',
   link: '.header__menu .link',
 }
+// hamburger mobile menu
+if (window.matchMedia('(max-width: 1023px)').matches) {
+  const main = () => {
+    const hamburger = document.querySelector(SELECTORS.hamburger)
+    const navMenu = document.querySelector(SELECTORS.navMenu)
+    const body = document.querySelector(SELECTORS.body)
+    const link = document.querySelectorAll(SELECTORS.link)
 
-const main = () => {
-  const hamburger = document.querySelector(SELECTORS.hamburger)
-  const navMenu = document.querySelector(SELECTORS.navMenu)
-  const body = document.querySelector(SELECTORS.body)
-  const link = document.querySelectorAll(SELECTORS.link)
+    const activeToggle = () => {
+      hamburger.classList.toggle(CLASSES.active)
+      navMenu.classList.toggle(CLASSES.active)
 
-  const activeToggle = () => {
-    hamburger.classList.toggle(CLASSES.active)
-    navMenu.classList.toggle(CLASSES.active)
-
-    if (navMenu.classList.contains(CLASSES.active)) {
-      // Disable scroll
-      // body.style.overflow = STYLES.hidden
-      body.classList.add(CLASSES.openMobileMenu)
-      navMenu.scrollTop = 0
-    } else {
-      // Enable scroll
-      // body.removeAttribute('style')
-      body.classList.remove(CLASSES.openMobileMenu)
+      if (navMenu.classList.contains(CLASSES.active)) {
+        // Disable scroll
+        // body.style.overflow = STYLES.hidden
+        body.classList.add(CLASSES.openMobileMenu)
+        navMenu.scrollTop = 0
+      } else {
+        // Enable scroll
+        // body.removeAttribute('style')
+        body.classList.remove(CLASSES.openMobileMenu)
+      }
     }
-  }
-  link.forEach((item) => {
-    item.addEventListener('click', () => {
+    link.forEach((item) => {
+      item.addEventListener('click', () => {
+        activeToggle()
+      })
+    })
+    hamburger.addEventListener('click', () => {
       activeToggle()
     })
-  })
-
-  hamburger.addEventListener('click', () => {
-    activeToggle()
-  })
+  }
+  main()
 }
-
-main()
--- src/js/_modal.js
+++ src/js/_modal.js
@@ -1,78 +1,78 @@
 const CLASSES = {
   activeModal: 'visible',
-  stopScrolling: 'modal-active',
 }
 
 const SELECTORS = {
   openModal: '.header__menu__item',
-  modal: '#modal',
-  modalBg: '.modal__background',
-  closeModal: '.modal__close',
+  modal: 'body > .modal',
+  modalBg: 'body > .modal .modal__background',
+  closeModal: 'body > .modal .modal__close',
 }
+if (window.matchMedia('(min-width: 1024px)').matches) {
+  const main = () => {
+    const btnModal = document.querySelectorAll(SELECTORS.openModal)
+    const openModal = btnModal[btnModal.length - 1]
+    const modal = document.querySelector(SELECTORS.modal)
+    const modalBg = document.querySelector(SELECTORS.modalBg)
+    const closeModal = document.querySelector(SELECTORS.closeModal)
 
-const main = () => {
-  // donation button
-  const openModal = document.querySelectorAll(SELECTORS.openModal)[
-    document.querySelectorAll(SELECTORS.openModal).length - 1
-  ]
-
-  const modal = document.querySelector(SELECTORS.modal)
-  const modalBg = document.querySelector(SELECTORS.modalBg)
-  const closeModal = document.querySelector(SELECTORS.closeModal)
-
-  // open modal
-  openModal.addEventListener('click', function (event) {
-    event.preventDefault()
-    showModal()
-  })
-
-  // hide modal
-  modalBg.addEventListener('click', function (event) {
-    hideModal()
-  })
-
-  closeModal.addEventListener('click', function (event) {
-    event.preventDefault()
-    hideModal()
-  })
-
-  const showModal = () => {
-    modal.classList.add(CLASSES.activeModal)
-    // body.classList.add(CLASSES.stopScrolling)
-
-    // autofocus on X icon
-    closeModal.focus()
-
-    // close modal when press esc key
-    document.addEventListener('keydown', function (e) {
-      if (e.key === 'Escape') {
-        hideModal()
-        enableScroll()
-      }
+    // open modal
+    openModal.addEventListener('click', function (event) {
+      event.preventDefault()
+      showModal()
     })
 
-    //disable scroll
-    disableScroll()
-  }
+    // hide modal
+    modalBg.addEventListener('click', function (event) {
+      event.preventDefault()
+      hideModal()
+    })
 
-  const hideModal = () => {
-    modal.classList.remove(CLASSES.activeModal)
-    enableScroll()
-  }
+    closeModal.addEventListener('click', function (event) {
+      event.preventDefault()
+      hideModal()
+    })
 
-  const disableScroll = () => {
-    // Get the current page scroll position
-    let xScrollPosition = window.scrollX
-    let yScrollPosition = window.scrollY
+    // disable & enable scroll when dmodal is visible
 
-    window.onscroll = function () {
-      window.scrollTo(xScrollPosition, yScrollPosition)
+    const disableScroll = () => {
+      // Get the current page scroll position
+      let xScrollPosition = window.scrollX
+      let yScrollPosition = window.scrollY
+
+      window.onscroll = function () {
+        window.scrollTo(xScrollPosition, yScrollPosition)
+      }
+    }
+
+    const enableScroll = () => {
+      window.onscroll = function () {}
+    }
+
+    // show & hide modal
+    const showModal = () => {
+      modal.classList.add(CLASSES.activeModal)
+
+      // autofocus on X icon
+      closeModal.focus()
+
+      // close modal when press esc key
+      document.addEventListener('keydown', function (e) {
+        if (e.key === 'Escape') {
+          hideModal()
+          enableScroll()
+        }
+      })
+
+      //disable scroll
+      disableScroll()
+    }
+
+    const hideModal = () => {
+      modal.classList.remove(CLASSES.activeModal)
+      enableScroll()
     }
   }
 
-  const enableScroll = () => {
-    window.onscroll = function () {}
-  }
+  main()
 }
-
-main()
