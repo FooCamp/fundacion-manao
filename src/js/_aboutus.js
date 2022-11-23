64e25052f6908e36a3ad805466b666b0a802dd99
Sebastian Benitez <jsb1439@hotmail.com>
2022-11-23 15:42:48 -0500

    Fix js errors when components are not present in the page

--- src/js/_aboutus.js
+++ src/js/_aboutus.js
@@ -14,19 +14,21 @@ const main = () => {
     return item.querySelector('.quote__person_card').innerHTML
   }
 
-  const person_cards = document.querySelectorAll('.person_card__container')
-  // Default quote
-  setBlockQuote(getQuoteText(person_cards[0]))
-  // Add event listener per card
-  person_cards.forEach(function (item) {
-    item.addEventListener('mouseenter', () => {
-      // Write card quote
-      setBlockQuote(getQuoteText(item))
+  if (main_quote) {
+    const person_cards = document.querySelectorAll('.person_card__container')
+    // Default quote
+    setBlockQuote(getQuoteText(person_cards[0]))
+    // Add event listener per card
+    person_cards.forEach(function (item) {
+      item.addEventListener('mouseenter', () => {
+        // Write card quote
+        setBlockQuote(getQuoteText(item))
+      })
+      item.addEventListener('mouseleave', () => {
+        // Write default quote
+        setBlockQuote(getQuoteText(person_cards[0]))
+      })
     })
-    item.addEventListener('mouseleave', () => {
-      // Write default quote
-      setBlockQuote(getQuoteText(person_cards[0]))
-    })
-  })
+  }
 }
 main()
