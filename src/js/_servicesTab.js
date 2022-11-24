b0a656f624ee9b783355d2470e0725a06001e5cd
Jordy Vasquez <iq.mecatronica@gmail.com>
2022-11-24 18:17:44 -0500

    [feature] Add new model service tabs

--- src/js/_servicesTab.js
+++ src/js/_servicesTab.js
@@ -3,47 +3,38 @@ const CLASSES = {
 }
 
 const SELECTORS = {
-  tabN: '.service-tabs__tab--n',
-  tabI: '.service-tabs__tab--i',
+  tabNames: '.service-tabs__name',
+  tabContent: '.service-tabs__content',
   tNames: '.tab__name',
-  tImage: '.tab__image',
+  tContent: '.tab__content',
 }
 const main = () => {
-  const tabN = document.querySelector(SELECTORS.tabN)
+  const tabNames = document.querySelector(SELECTORS.tabNames)
 
   // conditional to prevent error when component is missing
-  if (!tabN) {
+  if (!tabNames) {
     return
   }
 
-  const tabI = document.querySelector(SELECTORS.tabI)
-  const namesErase = tabI.querySelectorAll(SELECTORS.tNames)
-  const imagesErase = tabN.querySelectorAll(SELECTORS.tImage)
-  const names = tabN.querySelectorAll(SELECTORS.tNames)
-  const images = tabI.querySelectorAll(SELECTORS.tImage)
+  const tabContent = document.querySelector(SELECTORS.tabContent)
 
-  // Clean items that are not going to be used
-  namesErase.forEach((item) => {
-    item.remove()
-  })
-  imagesErase.forEach((item) => {
-    item.remove()
-  })
+  const names = tabNames.querySelectorAll(SELECTORS.tNames)
+  const content = tabContent.querySelectorAll(SELECTORS.tContent)
 
   // Assigns the 'active' class to the first element
   names[0].classList.toggle(CLASSES.active)
-  images[0].classList.toggle(CLASSES.active)
+  content[0].classList.toggle(CLASSES.active)
 
   const removeClass = () => {
     names.forEach((empty, i) => {
       names[i].classList.remove(CLASSES.active)
-      images[i].classList.remove(CLASSES.active)
+      content[i].classList.remove(CLASSES.active)
     })
   }
 
   const activeClass = (i) => {
     names[i].classList.add(CLASSES.active)
-    images[i].classList.add(CLASSES.active)
+    content[i].classList.add(CLASSES.active)
   }
 
   names.forEach((currentTab, i) => {
