790db835ef5390340c667fe9d2272c69919f423b
Ana Perez <annamarriaperez@gmail.com>
2022-11-23 20:33:49 -0500

    Keeping  focus inside modal

--- src/js/_modal.js
+++ src/js/_modal.js
@@ -16,6 +16,13 @@ if (window.matchMedia('(min-width: 1024px)').matches) {
     const modalBg = document.querySelector(SELECTORS.modalBg)
     const closeModal = document.querySelector(SELECTORS.closeModal)
 
+    // add all the elements inside modal which you want to make focusable
+
+    const focusableElements = 'button, [href], [tabindex]:not([tabindex="-1"])'
+    const firstFocusableElement = modal.querySelectorAll(focusableElements)[0] // get first element to be focused inside modal
+    const focusableContent = modal.querySelectorAll(focusableElements)
+    const lastFocusableElement = focusableContent[focusableContent.length - 1] // get last element to be focused inside modal
+
     // open modal
     openModal.addEventListener('click', function (event) {
       event.preventDefault()
@@ -52,6 +59,30 @@ if (window.matchMedia('(min-width: 1024px)').matches) {
     // show & hide modal
     const showModal = () => {
       modal.classList.add(CLASSES.activeModal)
+
+      // focus trapped inside modal
+      document.addEventListener('keydown', function (e) {
+        let isTabPressed = e.key === 'Tab' || e.keyCode === 9
+
+        if (!isTabPressed) {
+          return
+        }
+
+        if (e.shiftKey) {
+          // if shift key pressed for shift + tab combination
+          if (document.activeElement === firstFocusableElement) {
+            lastFocusableElement.focus() // add focus for the last focusable element
+            e.preventDefault()
+          }
+        } else {
+          // if tab key is pressed
+          if (document.activeElement === lastFocusableElement) {
+            // if focused has reached to last focusable element then focus first focusable element after pressing tab
+            firstFocusableElement.focus() // add focus for the first focusable element
+            e.preventDefault()
+          }
+        }
+      })
 
       // autofocus on X icon
       closeModal.focus()
